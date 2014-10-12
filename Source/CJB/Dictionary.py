f7757f7a9765cd045ad22b5a6735353394252a54
uwadminuser <catchumfish@aol.com>
2014-10-12 01:23:53 -0600

    added comments and thoughts for future brainstorming

--- Source/CJB/Dictionary.py
+++ Source/CJB/Dictionary.py
@@ -1,10 +1,27 @@
 #   Dictonary.py
 
-#   DESCRIPTION HERE
+#   Uses a dictionary file to attempt to crack the hash.
+#   Simply reads through the file line by line, and tries each entry.
+#   Possible source for passwords are 'top most used passwords' lists
 
-#   Chris Bugg
-#   10/7/14
+# Updated on 10/12/2014:
+#   Chris Hamm is working on this section.
+#   added in comments and some thoughts
 
 class Dictionary():
+    # needs to take parameters like lowercase alpha, uppercase alpha, numerical. special symbols, etc
 
+    #class variables
     done = False
+
+    #need to create constructor
+        #create flags for local variables
+            #possible flags
+                #a-z
+                #A-Z
+                #0-9
+                #special symbols !@#$%^&*()_+-=\|/ etc
+                #All ASCII characters (includes all of the above)
+    #need to correct import file based on set flag values
+    #need to use password read from file and try to get the matching hash until found or end of file
+    #needs to output progress also
