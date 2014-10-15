8577b92232661c979ed58cac35cd74310c5d5a96
Chris Bugg <cbugg@uwyo.edu>
2014-10-15 14:11:26 -0600

    Update Controller with mostly functioning text-gui to simulate actual gui till that gets done/integrated, Took control of dictionary class and started work on it. --CJB

--- Source/CJB/Dictionary.py
+++ Source/CJB/Dictionary.py
@@ -7,13 +7,6 @@
 # Updated on 10/12/2014:
 #   Chris Hamm is working on this section.
 #   added in comments and some thoughts
-
-class Dictionary():
-    # needs to take parameters like lowercase alpha, uppercase alpha, numerical. special symbols, etc
-
-    #class variables
-    done = False
-
     #need to create constructor
         #create flags for local variables
             #possible flags (RECOMMEND USING SWITCH-CASE STATEMENTS HERE)
@@ -25,3 +18,59 @@ class Dictionary():
     #need to correct import file based on set flag values
     #need to use password read from file and try to get the matching hash until found or end of file
     #needs to output progress also
+
+#   Updated on 10/15/14:
+#   Chris Bugg is working on this class now
+
+#Imports
+import hashlib
+
+class Dictionary():
+
+    #class variables
+    done = False
+    algorithm = ""
+    fileName = ""
+    hash = ""
+    status = ""
+    found = False
+
+    #Constructor
+    def __init__(self):
+
+        x=1
+
+    #Sets algorithm to be used
+    def setAlgorithm(self, algorithm):
+
+        x=1
+
+    #Sets the dictionary file's name
+    def setFileName(self, fileName):
+
+        x=1
+
+    #Sets the original hash we're looking for
+    def setHash(self, hash):
+
+        x=1
+
+    #Actually finds the hash in the file (hopefully)
+    def find(self):
+
+        x=1
+
+    #Returns T/F if done searching or not
+    def done(self):
+
+        x=1
+
+    #Returns status summary of searching so far
+    def status(self):
+
+        x=1
+
+    #
+    def found(self):
+
+        x=1
