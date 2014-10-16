5abc1b5c6977bc8d9160d03462fec5db85e6e4b8
chris <catchumfish@aol.com>
2014-10-15 22:11:18 -0600

    updating server network file

--- Source/CJB/NetworkDemoServer.py
+++ Source/CJB/NetworkDemoServer.py
@@ -1,30 +1,15 @@
 __author__ = 'chris'
 #TESTING ****************ONLY************
-import SocketServer
+import socket #import the socket module
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+s = socket.socket() #Create a socket object
+port = 12397 # Reserve a port for your service
+s.bind(('',port)) #Bind to the port
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+s.listen(5) #Wait for the client connection
+while True:
+    c,addr = s.accept() #Establish a connection with the client
+    print "Got connection from", addr
+    c.send("Thank you for connecting!")
+    c.send("crunch a 6 char password from aaaaaa to aa9999")
+    c.close()
