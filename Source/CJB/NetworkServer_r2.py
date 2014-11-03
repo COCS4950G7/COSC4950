9c77c189c9c2bde52167090c8a5914976ce80574
uwadminuser <catchumfish@aol.com>
2014-11-02 23:18:53 -0700

    Added a try exception clause to catch any exeptions that are not caught by the other try except clauses

--- Source/CJB/NetworkServer_r2.py
+++ Source/CJB/NetworkServer_r2.py
@@ -14,6 +14,10 @@ __author__ = 'Chris_Hamm'
             #(UNTESTED) If server throws an exception, it sends a message to all nodes before closing the socket
             #(UNTESTED) Server prompts user to select what type of cracking method you will be performing
             #(UNTESTED) Server informs the client nodes about what cracking mode the server is running
+            #(UNTESTED) Additonal try and except clause have been added to handle errors that are thrown in a section that is not covered by another try except clause
+
+    #UNSOLVED ERROR
+        #Somehow the socket was not closed properly and I was not able to run the server code on my computer again until I restarted my computer
 
 #STARTUP SERVER#########################
 import socket #import the socket module
@@ -21,7 +25,7 @@ socketObject= socket.socket() #create a socket object
 port= 12397 #reserving a port for the service
 socketObject.bind(('',port)) #bind to port
 
-#PROMPT USER FOR NUMBER OF NODES AND WHAT MODE (BF, DICTIONARY,ETC)##############################
+#PROMPT USER FOR NUMBER OF NODES##############################
 numOfNodes = int(raw_input('How many nodes will there be? ')) #store how many nodes there will be
 print ("Number of Nodes: " + str(numOfNodes)); #tell user how many nodes have been set to wait for
 print ("Now waiting for nodes to connect.");
@@ -32,51 +36,55 @@ typeOfCracking = int(raw_input('What cracking method are you using? (Enter the a
                                '2) Dictionary \n' #2 represents Dictionary Attacks
                                '3) Rainbow Tables')) #3 represents Rainbow Tables
 
-
-#(client-side task: startup the nodes
-#(client-side task: say "hi" to server)
 #WAIT FOR ALL NODES TO CONNECT#######################
 numOfConnectedNodes = int(0) #a counter to count how many nodes have connected
 listOfConnectedIPAddresses = []; #contains the ip addresses of all connected nodes
 socketObject.listen(5) #wait for client connection
-while True:
-    c,addr = socketObject.accept() #Establish a connection with the client
-    print "Got connection from", addr
-    listOfConnectedIPAddresses.append(addr) #add the node's ip address to the list
-    c.send("Your IP address (" + str(addr) + ") has been added to the servers List!")
-    print ("The node IP " + str(addr) + " has been added to the listOfConnectedIPAddresses");
-    #TELL CLIENTS WHAT MODE THE SERVER IS RUNNING
-    if(typeOfCracking == 1):
-        c.send("Server is in Brute-Force Mode");
-    elif(typeOfCracking == 2):
-        c.send("Server is in Dictionary Mode");
-    elif(typeOfCracking == 3):
-        c.send("Server is in Rainbow Table Mode");
-    else:
-        print("ERROR: invalid typeOfCracking value");
-    c.send("Thank you for connecting!")
-    numOfConnectedNodes += 1 #increment numOfConnectedNodes
-
-    #ARE ALL NODES CONNECTED?#####################
-    try:
-        #IF NO, CONTINUE TO WAIT
-        if(numOfNodes - numOfConnectedNodes > 0):
-            c.send("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect." )
-            print("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect.");
-        #(client side task: wait for server to reply)
-        #IF YES,THEN SAY "return hi" TO ALL NODES
+try:
+    while True:
+        c,addr = socketObject.accept() #Establish a connection with the client
+        print "Got connection from", addr
+        listOfConnectedIPAddresses.append(addr) #add the node's ip address to the list
+        c.send("Your IP address (" + str(addr) + ") has been added to the servers List!")
+        print ("The node IP " + str(addr) + " has been added to the listOfConnectedIPAddresses");
+        #TELL CLIENTS WHAT MODE THE SERVER IS RUNNING
+        if(typeOfCracking == 1):
+            c.send("Server is in Brute-Force Mode");
+        elif(typeOfCracking == 2):
+            c.send("Server is in Dictionary Mode");
+        elif(typeOfCracking == 3):
+            c.send("Server is in Rainbow Table Mode");
         else:
-            print("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect.");
-            print("All nodes have connected to the server!");
-            c.close() #OMIT THIS IN FUTURE!!!!
-    except Exception as inst:
-        print ("An Exception was thrown");
-        c.send("Server: An Exception has been thrown. Server shutting down."); #display message to all nodes that the server had an error
-        print type(inst) #the exception instance
-        print inst.args #srguments stored in .args
-        print inst #_str_ allows args tto be printed directly
-        c.close()
+            print("ERROR: invalid typeOfCracking value");
+        c.send("Thank you for connecting!")
+        numOfConnectedNodes += 1 #increment numOfConnectedNodes
 
+        #ARE ALL NODES CONNECTED?#####################
+        try:
+            #IF NO, CONTINUE TO WAIT
+            if(numOfNodes - numOfConnectedNodes > 0):
+                c.send("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect." )
+                print("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect.");
+            #(client side task: wait for server to reply)
+            #IF YES,THEN SAY "return hi" TO ALL NODES
+            else:
+                print("Still waiting for " + str(numOfNodes - numOfConnectedNodes) + " nodes to connect.");
+                print("All nodes have connected to the server!");
+                #c.close() #TEMPORARY CLOSE, UNCOMMENT ONLY WHEN NEEDED
+        except Exception as inst:
+            print ("An Exception was thrown");
+            c.send("Server: An Exception has been thrown. Server shutting down."); #display message to all nodes that the server had an error
+            print type(inst) #the exception instance
+            print inst.args #srguments stored in .args
+            print inst #_str_ allows args tto be printed directly
+            c.close()
+except Exception as inst:  #exception condition for primary try statement
+    print("An Exception has occured");
+    c.send("Server: An Exception has occured. Server shutting down.");
+    print type(inst) #the exception instance
+    print inst.args #arguments stored in .args
+    print inst #_str_ allows args to  be printed directly
+    c.close()
 
 #SPLIT WORKLOAD INTO MANY EQUALLY SIZED PIECES###############################
 
