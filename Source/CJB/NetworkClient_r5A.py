0d42f3d41147d977bd13e9772b60acf5583d641a
uwadminuser <catchumfish@aol.com>
2014-11-11 18:17:21 -0700

    Added a few more comments to NetworkClient_r5.
    NetworkClient_r5A and NetworkServer_r5A have easier to read error messages. These where used as my trial and error versions to try to fix the hide debugging messages in the client. Unfortunately this still has not been fixed.

--- Source/CJB/NetworkClient_r5A.py
+++ Source/CJB/NetworkClient_r5A.py
@@ -1,10 +1,13 @@
 __author__ = 'chris hamm'
-#Created on 11/10/2014
-#NetwrokClient_r5
+#Created on 11/11/2014
+#NetwrokClient_r5A
 
-#This is designed to run with NetworkServer_r5 AND NetworkServer_r5_withThreading
+#This is designed to run with NetworkServer_r5A
 #This revision is designed to use a single socket, and just change the ports
 
+#STILL NEEDS TO HAVE THE CLIENT TELLS THE SERVER THAT IT CRASHED!!!!!!
+#STILL NEED TO LISTEN TO SEE IF SERVER CRASHED!!!!!!!!!!!
+#OUT OF SYNC WITH THE SERVER WHEN DEBUGGING MESSAGES ARE TURNED OFF
 #Areas marked with @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ are areas that need to be changed to work in a more optimized fashion
 
 #MASTER TRY BLOCK
@@ -13,13 +16,13 @@ try:
     port= 49200
     clientSocket= socket.socket()
     #set the default timeout for trying to connect to a socket
-    clientSocket.settimeout(20.0) #will time out after 20 seconds
+    clientSocket.settimeout(15.0) #will time out after 15 seconds
     print("clientSocket was sucsessfully created");
 
     #prompt user if they want debugging messages turned on
     showDebuggingMessages= str(raw_input('Do you want to display the debugging messages? (Enter in corresponding numeral) \n'
                                          '1) Yes \n'
-                                         '2) No (This option does not work, it throws an error and crashes) \n'))
+                                         '2) No (This option DOES NOT WORK, it throws an error and CRASHES) \n'))
     if(showDebuggingMessages=="1"):
         print("Debugging messages are turned ON");
     elif(showDebuggingMessages=="2"):
@@ -38,10 +41,13 @@ try:
         print("========================================================================================");
         print("ERROR: the connection has timed out. Check to see if you entered the correct IP Address.");
         print("Error code: " + str(msg[0]) + " Message: " + msg[1]);
+        print("Socket timeout set to: " + clientSocket.gettimeout + " seconds");
+        print("========================================================================================");
     except socket.error as msg:
         print("========================================================================================");
         print("ERROR: Failed to connect to server");
         print("Error code: " + str(msg[0]) + " Message: " + msg[1]);
+        print("========================================================================================");
 
     #print server connection verification message, then send client verification message
     if(showDebuggingMessages=="1"):
@@ -64,6 +70,13 @@ try:
                 print "Received instructions: " + str(clientSocket.recv(1024))
             else:
                 myNewInstructions= clientSocket.recv(1024)
+        except socket.timeout as msg:
+            print("========================================================================================");
+            print("ERROR: the connection has timed out. Thrown from inside request NEXT instruction set try block");
+            print("Error code: " + str(msg[0]) + " Message: " + msg[1]);
+            print("Socket timeout set to: " + clientSocket.gettimeout + " seconds");
+            serverSaysKeepSearching= False
+            print("========================================================================================");
         except Exception as inst:
             print("========================================================================================");
             print("ERROR: Exception thrown in request NEXT instruction set try block");
@@ -71,6 +84,7 @@ try:
             print inst.args #srguments stored in .args
             print inst #_str_ allows args tto be printed directly
             serverSaysKeepSearching= False
+            #raise Exception("manual error 2 being thrown")
             print("========================================================================================");
 
         #check for the DONE command from the server
@@ -86,20 +100,33 @@ try:
                 print("Program has finished by the DONE command");
             else:
                 print("server command is not DONE");
+        except socket.timeout as msg:
+            print("========================================================================================");
+            print("ERROR: the connection has timed out. Thrown inside check for servers DONE command try block");
+            print("Error code: " + str(msg[0]) + " Message: " + msg[1]);
+            print("Socket timeout set to: " + clientSocket.gettimeout + " seconds");
+            serverSaysKeepSearching= False
+            print("========================================================================================");
         except Exception as inst:
+            print("========================================================================================");
             print("ERROR: An error has occurred in the check for servers DONE command try block");
             print type(inst) #the exception instance
             print inst.args #srguments stored in .args
             print inst #_str_ allows args tto be printed directly
+            serverSaysKeepSearching= False
+            #raise Exception("manual error 1 being thrown")
+            print("========================================================================================");
 
 
 
 #MASTER EXCEPTION BLOCK
 except Exception as inst:
+    print("========================================================================================");
     print("An Exception has been thrown in the Master Try Block");
     print type(inst) #the exception instance
     print inst.args #srguments stored in .args
     print inst #_str_ allows args tto be printed directly
+    print("========================================================================================");
 #MASTER FINALLY BLOCK
 finally:
     print("Closing socket");
