4aabafa6850190aa9f589a04ab96a20a712c5d17
uwadminuser <catchumfish@aol.com>
2014-12-06 17:13:24 -0700

    added a base file for the next revision of network files. I am still waiting for the other Chris to finish editting his controller class.

---
+++ Source/Network/NetworkServer_r9.py
@@ -0,0 +1,10 @@
+__author__ = 'Chris Hamm'
+#NetworkServer_r9
+#Created: 12/5/2014
+
+############################################################################
+#REVISION NOTES:
+#   This version is designed to replace revision 7B
+#   Also to try to fix the automatic startup problem when imported
+###########################################################################
+
