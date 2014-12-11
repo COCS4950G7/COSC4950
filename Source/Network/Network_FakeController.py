da2a96b718f77fe238a58f2166964d764db91204
uwadminuser <catchumfish@aol.com>
2014-12-11 00:43:26 -0700

    added comment, having problem with the fakeController not automatically starting up like it is supposed to. Making testing impossible.

--- Source/Network/Network_FakeController.py
+++ Source/Network/Network_FakeController.py
@@ -4,6 +4,8 @@ __author__ = 'Chris Hamm'
 #This is used to simulate the controller class since i cant modify the existing one
 #and I need to test the Network classes
 
+#PROBLEM: This class does not automatically startup like it is supposed to!!!
+
 from time import time
 import sys
 import os
@@ -12,41 +14,50 @@ from multiprocessing import Process, Pipe, Lock
 import NetworkClient_r9
 import NetworkServer_r9
 
-class Controller():
-    #class variables
-    selectingTheNetworkClass = True
-    selectedNetworkClass = 0
-    networkServer = 0
-    networkClient = 0
-    #lock = Lock()
-    controllerPipe, networkPipe = Pipe()
+try:
+    class Network_FakeController():
+        #class variables
+        selectingTheNetworkClass = True
+        selectedNetworkClass = 0
+        networkServer = 0
+        networkClient = 0
 
-    #defining network sub-processes
-    networkServer = Process(target=NetworkServer_r9.NetworkServer, args=(networkPipe,))
-    networkClient = Process(target=NetworkClient_r9.NetworkClient, args=(networkPipe,))
+        #lock = Lock()
+        controllerPipe, networkPipe = Pipe()
 
-    #constructor
-    def __init__(self):
+        #defining network sub-processes
+        networkServer = Process(target=NetworkServer_r9.NetworkServer, args=(networkPipe,))
+        networkClient = Process(target=NetworkClient_r9.NetworkClient, args=(networkPipe,))
 
-        print " "
-        print "Starting up the FakeController class"
-        print " "
+        #constructor
+        def __init__(self):
 
-        #ask which network class you want to start up
-        while(self.selectingTheNetworkClass == True):
-            self.selectedNetworkClass = str(raw_input('Which network class you want to start? (Enter in the corresponding numeral): \n'
-                                                      '1) NetworkServer \n'
-                                                      '2) NetworkClient \n'))
-            if(self.selectedNetworkClass == "1"):
-                print "Starting NetworkServer..."
-                self.networkServer.start()
-                self.selectingTheNetworkClass = False
-            elif(self.selectedNetworkClass == "2"):
-                print "Starting NetworkClient..."
-                self.networkClient.start()
-                self.selectingTheNetworkClass = False
-            else:
-                print "ERROR: Invalid input"
-                print " "
+            print " "
+            print "Starting up the FakeController class"
+            print " "
 
-        #end of while loop
+            #ask which network class you want to start up
+            while self.selectingTheNetworkClass == True:
+                self.selectedNetworkClass = str(raw_input('Which network class you want to start? (Enter in the corresponding numeral): \n'
+                                                          '1) NetworkServer \n'
+                                                          '2) NetworkClient \n'))
+                if(self.selectedNetworkClass == "1"):
+                    print "Starting NetworkServer..."
+                    self.networkServer.start()
+                    self.selectingTheNetworkClass = False
+                elif(self.selectedNetworkClass == "2"):
+                    print "Starting NetworkClient..."
+                    self.networkClient.start()
+                    self.selectingTheNetworkClass = False
+                else:
+                    print "ERROR: Invalid input"
+                    print " "
+
+            #end of while loop
+except Exception as inst:
+    print "========================================================================================"
+    print "ERROR: An Exception was thrown in the Master Try Block of Controller class in FakeController"
+    print type(inst) #the exception instance
+    print inst.args #srguments stored in .args
+    print inst #_str_ allows args tto be printed directly
+    print "========================================================================================"
