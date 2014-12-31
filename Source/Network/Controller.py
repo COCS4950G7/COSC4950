8311d67ca1699e573da59660324bad0b8adaffb1
uwadminuser <catchumfish@aol.com>
2014-12-31 15:24:34 -0700

    Modified the network version of controller to use NetworkServer_r9A and NetworkClient_r9A.

--- Source/Network/Controller.py
+++ Source/Network/Controller.py
@@ -30,8 +30,8 @@ from multiprocessing import Process, Pipe, Lock
 #import RainbowMaker
 import Dictionary
 #import Brute_Force
-import NetworkClient_r9
-import NetworkServer_r9
+import NetworkClient_r9A
+import NetworkServer_r9A
 import Chunk
 
 class Controller():
@@ -46,9 +46,9 @@ class Controller():
     controllerPipe, networkPipe = Pipe()
 
     #Defining network sub-processes as class variables that are instances of the network objects
-    networkServer = Process(target=NetworkServer_r9.NetworkServer, args=(networkPipe,))
+    networkServer = Process(target=NetworkServer_r9A.NetworkServer, args=(networkPipe,))
     #networkClient = Process(target=NetworkClient.NetworkClient(networkPipe))
-    networkClient = Process(target=NetworkClient_r9.NetworkClient, args=(networkPipe,))
+    networkClient = Process(target=NetworkClient_r9A.NetworkClient, args=(networkPipe,))
 
     #Initializing variable to a default value
     serverIP = "127.0.1.1"
