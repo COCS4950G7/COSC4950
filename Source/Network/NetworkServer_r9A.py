a6b884067d856776d59883bd4a745a0302419cf2
uwadminuser <catchumfish@aol.com>
2014-12-31 14:39:18 -0700

    NetworkServer_r9A is attempting to detect linux machine's ip address, attempt #4. retrieving the network interface , then attempting to retrieve the ip.

--- Source/Network/NetworkServer_r9A.py
+++ Source/Network/NetworkServer_r9A.py
@@ -80,8 +80,27 @@ class NetworkServer(): #CLASS NAME WILL NOT CHANGE BETWEEN VERSIONS
                 if(platform.system()=="Windows"):
                     print socket.gethostbyname(socket.gethostname())
                 elif(platform.system()=="Linux"):
-                    socket.connect(('google.com',0))
-                    print socket.getsockname()
+                    import fcntl
+                    import struct
+                    import os
+
+                    def get_interface_ip(ifname):
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',ifname[:15]))[20:24])
+
+                    def get_lan_ip():
+                        ip = socket.gethostbyname(socket.gethostname())
+                        if ip.startswith("127.") and os.name != "nt":
+                            interfaces = ["eth0","eth1","eth2","wlan0","wlan1","wifi0","ath0","ath1","ppp0"]
+                            for ifname in interfaces:
+                                try:
+                                    ip = get_interface_ip(ifname)
+                                    break
+                                except IOError:
+                                    pass
+                        return ip
+                    #end of def
+                    print get_lan_ip()
                 elif(platform.system()=="Darwin"):
                     print socket.gethostbyname(socket.gethostname())
                 else:
