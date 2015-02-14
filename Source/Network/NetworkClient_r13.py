428a2f5d27f54b6482074a58e48512d8f3828ad0
uwadminuser <catchumfish@aol.com>
2015-02-14 10:46:41 -0700

    added connected to server command in start up

--- Source/Network/NetworkClient_r13.py
+++ Source/Network/NetworkClient_r13.py
@@ -469,6 +469,7 @@ class NetworkClient():
         try: #connect to server try block
             clientSocket.connect((self.serverIPAddress, self.port))
             print "Connected to server\n"
+            sendConnectedCommandToController(self)
         except Exception as inst:
             print "===================================================================\n"
             print "Error in connect to server try block: " + str(inst) +"\n"
