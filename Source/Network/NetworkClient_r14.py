2868f0c324032625180386ae21597ef844e50a7a
uwadminuser <catchumfish@aol.com>
2015-02-16 15:19:44 -0700

    simple modified version of Nicks multiprocess version of server and client

---
+++ Source/Network/NetworkClient_r14.py
@@ -0,0 +1,97 @@
+__author__ = 'chris hamm'
+#NetworkClient_r14
+
+
+from multiprocessing.managers import SyncManager
+import Dictionary
+import Queue
+import Chunk
+import time
+
+IP = "192.168.1.103"
+PORTNUM = 22536
+AUTHKEY = "Popcorn is awesome!!!"
+
+
+def runclient():
+    try: #runclient definition try block
+        manager = make_client_manager(IP, PORTNUM, AUTHKEY)
+        job_q = manager.get_job_q()
+        result_q = manager.get_result_q()
+        dictionary = Dictionary.Dictionary()
+
+        while True:
+            try:
+                job = job_q.get_nowait()
+                chunk = Chunk.Chunk()
+                chunk.params = job.value['params']
+                chunk.data = job.value['data']
+                print chunk.params
+                time.sleep(.01)
+                dictionary.find(chunk)
+                result = dictionary.isFound()
+                time.sleep(0.00)
+                if result:
+                    print "Hooray!"
+                    print "key is: " + dictionary.showKey()
+                    key = dictionary.showKey()
+                    result_q.put(("w", key))
+                   # result_q.put(("c", key))
+                    return
+                else:
+                    result_q.put(("f", chunk.params))
+                    #result_q.put(("c", chunk.params))
+            except Queue.Empty:
+                return
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in runclient definition try block"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
+
+class ServerQueueManager(SyncManager):
+    pass
+
+def make_client_manager(ip, port, authkey):
+    """ Create a manager for a client. This manager connects to a server on the
+        given address and exposes the get_job_q and get_result_q methods for
+        accessing the shared queues from the server.
+        Return a manager object.
+    """
+
+
+
+    ServerQueueManager.register('get_job_q')
+    ServerQueueManager.register('get_result_q')
+
+    manager = ServerQueueManager(address=(ip, port), authkey=authkey)
+    manager.connect()
+
+    print 'Client connected to %s:%s' % (ip, port)
+    return manager
+
+
+if __name__ == '__main__':
+    try: #Main
+        import time
+        start_time= time.time()
+        runclient()
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in Main"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
+    finally:
+        end_time= time.time() - start_time
+        print "Client ran for: "+str(end_time)+" seconds"
+
---
+++ Source/Network/NetworkServer_r14.py
@@ -0,0 +1,125 @@
+__author__ = 'chris hamm'
+#NetworkServer_r14
+
+from multiprocessing.managers import SyncManager
+
+import time
+import Queue
+
+import Dictionary
+
+IP = "192.168.1.103"
+PORTNUM = 22536
+AUTHKEY = "Popcorn is awesome!!!"
+
+dictionary = Dictionary.Dictionary()
+
+def runserver():
+    try: #runserver definition try block
+        # Start a shared manager server and access its queues
+        manager = make_server_manager(PORTNUM, AUTHKEY)
+        shared_job_q = manager.get_job_q()
+        shared_result_q = manager.get_result_q()
+        dictionary.setAlgorithm('md5')
+        dictionary.setFileName("dic.txt")
+        dictionary.setHash("33da7a40473c1637f1a2e142f4925194") # popcorn
+
+        while not dictionary.isEof():
+
+            #chunk is a Chunk object
+            chunk = dictionary.getNextChunk() #get next chunk from dictionary
+            newChunk = manager.Value(dict, {'params': chunk.params, 'data': chunk.data})
+            shared_job_q.put(newChunk) #put next chunk on the job queue
+
+        while True:
+            result = shared_result_q.get()
+            if result[0] == "w":
+                print "The solution was found!"
+                key = result[1]
+                print "Key is: %s" % key
+                break
+            elif(result[0] == "c"):
+                print "A client has crashed!"
+            else: #f
+                print "Chunk finished with params: %s" %result[1]
+
+        # Sleep a bit before shutting down the server - to give clients time to
+        # realize the job queue is empty and exit in an orderly way.
+        time.sleep(2)
+        manager.shutdown()
+        return
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in runserver definition Try block"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
+ # This is based on the examples in the official docs of multiprocessing.
+    # get_{job|result}_q return synchronized proxies for the actual Queue
+    # objects.
+class JobQueueManager(SyncManager):
+    pass
+
+def make_server_manager(port, authkey):
+    """ Create a manager for the server, listening on the given port.
+        Return a manager object with get_job_q and get_result_q methods.
+    """
+    try: #Make_server_manager definition try block
+        job_q = Queue.Queue(maxsize=1000)
+        result_q = Queue.Queue()
+
+
+        try: #JobQueueManager/Lambda functions Try Block
+            JobQueueManager.register('get_job_q', callable=lambda: job_q)
+            JobQueueManager.register('get_result_q', callable=lambda: result_q)
+        except Exception as inst:
+            print "============================================================================================="
+            print "ERROR: An exception was thrown in Make_server_Manager: JobQueueManager/Lambda functions Try Block"
+            #the exception instance
+            print type(inst)
+            #srguments stored in .args
+            print inst.args
+            #_str_ allows args tto be printed directly
+            print inst
+            print "============================================================================================="
+
+        manager = JobQueueManager(address=(IP, port), authkey=authkey)
+        manager.start()
+        print 'Server started at port %s' % port
+        return manager
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in Make_server_manager definition Try block"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
+
+
+
+if __name__ == '__main__':
+    try: #Main
+        import  time
+        start_time= time.time()
+        runserver()
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in Main"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
+    finally:
+        end_time= time.time() - start_time
+        print "Server ran for "+str(end_time)+" seconds"
+
