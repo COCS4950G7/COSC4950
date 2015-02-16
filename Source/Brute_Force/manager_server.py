88037a74c1c7a6f76cab291a76ac3ee498375a11
uwadminuser <catchumfish@aol.com>
2015-02-16 11:27:00 -0700

    attempt 3

--- Source/Brute_Force/manager_server.py
+++ Source/Brute_Force/manager_server.py
@@ -12,36 +12,46 @@ AUTHKEY = "Popcorn is awesome!!!"
 dictionary = Dictionary.Dictionary()
 
 def runserver():
-    # Start a shared manager server and access its queues
-    manager = make_server_manager(PORTNUM, AUTHKEY)
-    shared_job_q = manager.get_job_q()
-    shared_result_q = manager.get_result_q()
-    dictionary.setAlgorithm('md5')
-    dictionary.setFileName("dic")
-    dictionary.setHash("33da7a40473c1637f1a2e142f4925194") # popcorn
+    try: #runserver definition try block
+        # Start a shared manager server and access its queues
+        manager = make_server_manager(PORTNUM, AUTHKEY)
+        shared_job_q = manager.get_job_q()
+        shared_result_q = manager.get_result_q()
+        dictionary.setAlgorithm('md5')
+        dictionary.setFileName("dic")
+        dictionary.setHash("33da7a40473c1637f1a2e142f4925194") # popcorn
 
-    while not dictionary.isEof():
+        while not dictionary.isEof():
 
-        #chunk is a Chunk object
-        chunk = dictionary.getNextChunk()
-        newChunk = manager.Value(dict, {'params': chunk.params, 'data': chunk.data})
-        shared_job_q.put(newChunk)
+            #chunk is a Chunk object
+            chunk = dictionary.getNextChunk()
+            newChunk = manager.Value(dict, {'params': chunk.params, 'data': chunk.data})
+            shared_job_q.put(newChunk)
 
-    while True:
-        result = shared_result_q.get()
-        if result[0] is "win":
-            key = result[1]
-            print "Key is: %s" % key
-            break
-        else:
-            print "Chunk finished with params: %s" %result[1]
+        while True:
+            result = shared_result_q.get()
+            if result[0] is "win":
+                key = result[1]
+                print "Key is: %s" % key
+                break
+            else:
+                print "Chunk finished with params: %s" %result[1]
 
-    # Sleep a bit before shutting down the server - to give clients time to
-    # realize the job queue is empty and exit in an orderly way.
-    time.sleep(2)
-    manager.shutdown()
-    return
-
+        # Sleep a bit before shutting down the server - to give clients time to
+        # realize the job queue is empty and exit in an orderly way.
+        time.sleep(2)
+        manager.shutdown()
+        return
+    except Exception as inst:
+        print "============================================================================================="
+        print "ERROR: An exception was thrown in runserver definition Try block"
+        #the exception instance
+        print type(inst)
+        #srguments stored in .args
+        print inst.args
+        #_str_ allows args tto be printed directly
+        print inst
+        print "============================================================================================="
  # This is based on the examples in the official docs of multiprocessing.
     # get_{job|result}_q return synchronized proxies for the actual Queue
     # objects.
@@ -52,17 +62,17 @@ def make_server_manager(port, authkey):
     """ Create a manager for the server, listening on the given port.
         Return a manager object with get_job_q and get_result_q methods.
     """
-    try:
+    try: #Make_server_manager definition try block
         job_q = Queue.Queue(maxsize=1000)
         result_q = Queue.Queue()
 
 
-        try:
+        try: #JobQueueManager/Lambda functions Try Block
             JobQueueManager.register('get_job_q', callable=lambda: job_q)
             JobQueueManager.register('get_result_q', callable=lambda: result_q)
         except Exception as inst:
             print "============================================================================================="
-            print "ERROR: An exception was thrown in Make_server_Manager: JobQueueManager/Lambda functions"
+            print "ERROR: An exception was thrown in Make_server_Manager: JobQueueManager/Lambda functions Try Block"
             #the exception instance
             print type(inst)
             #srguments stored in .args
@@ -77,7 +87,7 @@ def make_server_manager(port, authkey):
         return manager
     except Exception as inst:
         print "============================================================================================="
-        print "ERROR: An exception was thrown in Make_server_manager definition"
+        print "ERROR: An exception was thrown in Make_server_manager definition Try block"
         #the exception instance
         print type(inst)
         #srguments stored in .args
