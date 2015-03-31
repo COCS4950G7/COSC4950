f2dab20231d328053b39c5af58c6c683a167c5e4
NBaum26 <penguin@uwyo.edu>
2015-03-30 20:05:21 -0600

    updated to use 15b...finally...

--- Source/Network/test start client.py
+++ Source/Network/test start client.py
@@ -1,7 +1,7 @@
 # this is a shortcut method to run client without any UI. For debugging purposes only.
 
-from multiprocessing import Process
-from NetworkClient_r15a import Client
+from multiprocessing import Process, Event, Manager
+from NetworkClient_r15b import Client
 
 
 
@@ -9,14 +9,46 @@ class start():
 
     client = Process()
 
-
     def __init__(self):
         #self.start_client()
         return
 
-
     def start_client(self):
-        self.client = (Process(target=Client, args=("192.168.2.136",)))
+        #use a generic manager's shared dictionary to handle strings and ints
+        #Define the shared dictionary and it's values
+        manager = Manager()
+        dictionary = manager.dict()
+        dictionary["key"] = ''
+        dictionary["finished chunks"] = 0
+        dictionary["total chunks"] = 0
+        dictionary["server ip"] = "127.1.1.1"
+
+        #server/client/GUI signals shutdown when they're all done
+        shutdown = Event()
+        shutdown.clear()
+
+        #Define the various events
+        #server signals update when something has occurred (ie: chunk processed)
+        update = Event()
+        update.clear()
+
+        #client signals if it's connected or not
+        is_connected = Event()
+        is_connected.clear()
+
+        #client signals if it's doing stuff or not
+        is_doing_stuff = Event()
+        is_doing_stuff.clear()
+
+        #Shared is a list of shared events
+        shared = []
+        shared.append(dictionary)
+        shared.append(shutdown)
+        shared.append(update)
+        shared.append(is_connected)
+        shared.append(is_doing_stuff)
+
+        self.client = (Process(target=Client, args=("192.168.2.136", shared)))
         self.client.start()
         self.client.join()
         self.client.terminate()
