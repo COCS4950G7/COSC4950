aed1c6c8ef09b32579e36e9be3ba223708cc3f95
NBaum26 <penguin@uwyo.edu>
2015-04-15 11:41:40 -0600

    added basic support for some passlib algorithms.

--- Source/Network/HashGenerator.py
+++ Source/Network/HashGenerator.py
@@ -2,8 +2,21 @@
 # It takes in a string from the command line and outputs a hash for each algorithm in the python hashlib.
 
 import hashlib
+from passlib.context import CryptContext
+
+schemes = ["sha1_crypt", "sha256_crypt", "sha512_crypt", "md5_crypt",
+           "des_crypt", 'ldap_salted_sha1', 'ldap_salted_md5',
+           'ldap_sha1', 'ldap_md5', 'ldap_plaintext', "mysql323"]
+myctx = CryptContext(schemes)
 
 key = raw_input("enter key: ")
+print "hashlib hashes:\n"
 for algorithm in hashlib.algorithms:
     print "The %s hash representation of %s is: " % (algorithm, key)
     print hashlib.new(algorithm, key).hexdigest()
+
+print "\npasslib hashes:\n"
+
+for algorithm in schemes:
+    print "The %s hash representation of %s is: " % (algorithm, key)
+    print myctx.encrypt(key, algorithm)
